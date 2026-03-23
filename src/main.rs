a60324a0544db532fa1c6d2828ac332fd2c0fada
Mark Tyrkba <awfulmarco@gmail.com>
2026-03-23 19:57:57 +0300

    Remove closed TODO(#27): Switch statements

--- src/main.rs
+++ src/main.rs
@@ -63,7 +63,6 @@
 // TODO(#13): Debug info
 // TODO(#15): Stack unwind info
 // TODO(#14): GCC statement expressions
-// TODO(#27): Switch statements
 // TODO(#30): Ternary operator
 // TODO(#29): static assert
 
